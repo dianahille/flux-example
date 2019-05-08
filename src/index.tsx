ea258d2e66680959c3c09a58b37662d421041a39
Diana Hille <hille@b1-systems.de>
2019-05-08 18:12:10 +0200

    Bugfix: Delete unused Store in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import FruitActions from './flux/actions/FruitActions';
-import FruitStore from './flux/stores/FruitStore';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
