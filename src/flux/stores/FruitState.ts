08b1a0a82f28fb44a8363885a95d866f74199cc4
Diana Hille <hille@b1-systems.de>
2019-05-09 09:49:40 +0200

    Documentation: The documentation for FruitState.ts

--- src/flux/stores/FruitState.ts
+++ src/flux/stores/FruitState.ts
@@ -1,3 +1,14 @@
+/*
+ * The Fruit State
+ *
+ * A simple state for a Fruit Store. It tracks an
+ * array of strings.
+ *
+ * @author    Ralf Lang <lang@b1-systems.de>
+ * @category  React/Flux
+ * @package   Fruits
+ */ 
+
 export default class FruitState {
 
     public constructor(public fruits : string[]) {
