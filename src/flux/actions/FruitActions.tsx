fe7b0645d91a85f52477d14b5b5975a0de42fc13
Diana Hille <hille@b1-systems.de>
2019-05-09 10:00:49 +0200

    Documentation: Added documentation to FruitActions.ts

--- src/flux/actions/FruitActions.tsx
+++ src/flux/actions/FruitActions.tsx
@@ -1,7 +1,32 @@
+/*
+ * The Fruit Actions
+ *
+ * The action creator for the fruits application and the fruits store.
+ * Kepp all action types located here. The action creator should only
+ * focus on one part of the application.
+ *
+ * @author    Ralf Lang <lang@b1-systems.de>
+ * @category  React/Flux
+ * @package   Fruits
+ */ 
+
 import AppDispatcher from '../dispatcher/AppDispatcher';
+
+/*
+ * The action creator for the Fruit Store
+ *
+ * It currently only has the action 'ADD_FRUIT', which adds
+ * a new string element to the Fruit Store.
+ * 
+ * You need the Dispatcher here. Use the getInstance method to
+ * reuse a current dispatcher. Don't create a new one.
+ */
 
 class FruitActions
 {
+    /*
+     * Adds a new element to the store
+     */
     public static addFruit(newFruit: string): any
     {
         AppDispatcher.getInstance().dispatch({
